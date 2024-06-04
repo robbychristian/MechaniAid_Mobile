0bf6b4f58c49fb7e01a36896076c1f52c6d0387a
robbychristian <61650887+robbychristian@users.noreply.github.com>
2024-06-05 01:07:12 +0800

    Removed unnecessary files

--- src/navigation/DrawerNavigation.jsx
+++ src/navigation/DrawerNavigation.jsx
@@ -4,8 +4,6 @@ import React from "react";
 import Home from "../screen/Home";
 import { useDispatch } from "react-redux";
 import { Drawer, DrawerItem, IndexPath } from "@ui-kitten/components";
-import Request from "../screen/Request/Request";
-import AddRequest from "../screen/Request/AddRequest";
 
 const DrawerStack = createDrawerNavigator();
 
@@ -49,105 +47,16 @@ const DrawerNavigation = () => {
           color: "#fff",
         },
         headerTintColor: "#fff",
-        headerTitle: "MECHANI-AID", headerTitleAlign: "center"
+        headerTitle: "MECHANI-AID",
+        headerTitleAlign: "center",
       }}
     >
-      <DrawerStack.Screen
-        name="Home"
-        component={Home}
-      />
+      <DrawerStack.Screen name="Home" component={Home} />
       {/* <DrawerStack.Screen
         name="Services"
         component={Services}
         options={{ headerTitle: "SERVICES", headerTitleAlign: "center" }}
-      />
-      <DrawerStack.Screen
-        name="Announcement"
-        component={Announcement}
-        options={{ headerTitle: "ANNOUNCEMENTS", headerTitleAlign: "center" }}
-      />
-      <DrawerStack.Screen
-        name="Records"
-        component={Records}
-        options={{ headerTitle: "RECORDS", headerTitleAlign: "center" }}
-      />
-      <DrawerStack.Screen
-        name="Tanod"
-        component={Tanod}
-        options={{
-          headerTitle: "TANOD DEPLOYMENT",
-          headerTitleAlign: "center",
-        }}
-      />
-      <DrawerStack.Screen
-        name="DocumentSubmission"
-        component={DocumentSubmission}
-        options={{
-          headerTitle: "DOC. SUBMISSION",
-          headerTitleAlign: "center",
-        }}
-      />
-      <DrawerStack.Screen
-        name="BlotterReport"
-        component={BlotterReports}
-        options={{
-          headerTitle: "BLOTTER REPORT",
-          headerTitleAlign: "center",
-        }}
-      />
-      <DrawerStack.Screen
-        name="Reservations"
-        component={Reservations}
-        options={{
-          headerTitle: "RESERVATIONS",
-          headerTitleAlign: "center",
-        }}
-      />
-      <DrawerStack.Screen
-        name="Clinic"
-        component={Clinic}
-        options={{
-          headerTitle: "CLINIC",
-          headerTitleAlign: "center",
-        }}
-      />
-      <DrawerStack.Screen
-        name="Transactions"
-        component={Transaction}
-        options={{
-          headerTitle: "TRANSACTIONS",
-          headerTitleAlign: "center",
-        }}
-      />
-      <DrawerStack.Screen
-        name="BarangayRecords"
-        component={BarangayRecords}
-        options={{
-          headerTitle: "BRGY. RECORDS",
-          headerTitleAlign: "center",
-        }}
-      />
-      <DrawerStack.Screen
-        name="IncidentReports"
-        component={IncidentReports}
-        options={{
-          headerTitle: "INCIDENT REPORTS",
-          headerTitleAlign: "center",
-        }}
-      />
-      <DrawerStack.Screen
-        name="TransactionRecords"
-        component={TransactionRecords}
-        options={{
-          headerTitle: "TRANSACTION REC.",
-          headerTitleAlign: "center",
-        }}
-      />
-      <DrawerStack.Screen
-        name="BarangayNews"
-        component={BarangayNews}
-        options={{ headerTitle: "BARANGAY NEWS", headerTitleAlign: "center" }}
-      /> */}
+    /> */}
     </DrawerStack.Navigator>
   );
 };
